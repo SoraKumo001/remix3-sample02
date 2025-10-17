cab3bb4324409723cfa88190614b939d7f4c410a
SoraKumo <info@croud.jp>
2025-10-17 09:25:34 +0900

    feat: rename full-reload plugin to reload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     {
-      name: "full-reload",
+      name: "reload",
       handleHotUpdate({ server }) {
         server.moduleGraph.getModuleByUrl("/src/main.tsx").then((mod) => {
           if (mod) server.reloadModule(mod);
